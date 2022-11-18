400dacb040e3458b925049613b1bd303d8824e62
Francois BONNIN <francois.bonnin@ynov.com>
2022-11-18 09:25:27 +0100

    add layouts main & info

---
+++ src/app/layouts/layouts-info/layouts-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layouts-info',
+  templateUrl: './layouts-info.component.html',
+  styleUrls: ['./layouts-info.component.scss']
+})
+export class LayoutsInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

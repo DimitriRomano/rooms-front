f274f2acb8ee45bd4ae540de5ffc8a0076eba8b1
Francois BONNIN <francois.bonnin@ynov.com>
2022-11-18 10:57:36 +0100

    card hotel, no binding

---
+++ src/app/shared/card-hotel/card-hotel.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardHotelComponent } from './card-hotel.component';
+
+describe('CardHotelComponent', () => {
+  let component: CardHotelComponent;
+  let fixture: ComponentFixture<CardHotelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CardHotelComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CardHotelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SidebarClientComponent } from './sidebar-client/sidebar-client.component';
 import { AngularMaterialModule } from '../angular-material.module';
 import { LogoComponent } from './logo/logo.component';
+import { CardHotelComponent } from './card-hotel/card-hotel.component';
 
 @NgModule({
-  declarations: [SidebarClientComponent, LogoComponent],
-  exports: [SidebarClientComponent, LogoComponent],
+  declarations: [SidebarClientComponent, LogoComponent, CardHotelComponent],
+  exports: [SidebarClientComponent, LogoComponent, CardHotelComponent],
   imports: [CommonModule, AngularMaterialModule]
 })
 export class SharedModule {}

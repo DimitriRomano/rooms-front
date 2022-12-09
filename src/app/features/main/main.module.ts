71c1a8a0ba4189e0b8aaf7f0261e0e42df6a66f3
DimitriRomano <dimitri.romano09@gmail.com>
2022-12-09 10:31:19 +0100

    fix search bar

--- src/app/features/main/main.module.ts
+++ src/app/features/main/main.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { SharedModule } from '../../shared/shared.module';
 import { ListHotelComponent } from './list-hotel/list-hotel.component';
@@ -9,7 +10,7 @@ import { ListHotelComponent } from './list-hotel/list-hotel.component';
 @NgModule({
   declarations: [ListHotelComponent],
   exports: [ListHotelComponent],
-  imports: [CommonModule, SharedModule, HttpClientModule, MatFormFieldModule, MatInputModule],
+  imports: [CommonModule, SharedModule, HttpClientModule, MatFormFieldModule, MatInputModule,MatIconModule],
   providers: []
 })
 export class MainModule {}
